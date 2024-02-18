d0f2933aaf38b9871080f0ed117c67a612173680
Maherun Nessa <meherunela2002@gmail.com>
2024-02-18 17:59:20 +0600

    homepage

---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import HomeHero from "./components/home/components/HomeHero";
+
+export default function Home() {
+  return (
+    <main>
+      <HomeHero />
+    </main>
+  );
+}
