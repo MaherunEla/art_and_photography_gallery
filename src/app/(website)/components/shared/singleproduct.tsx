62608d3633c6d663b05aa057c1b72cea82a8516b
Maherun Nessa <meherunela2002@gmail.com>
2024-03-15 23:40:41 +0600

    without addcart

--- src/app/(website)/components/shared/singleproduct.tsx
+++ src/app/(website)/components/shared/singleproduct.tsx
@@ -136,7 +136,7 @@ const Singleproduct = ({ Gallery }: Props) => {
             <div className="flex gap-2.5">
               <button
                 onClick={() => {
-                  dispatch(addCart(product));
+                  // dispatch(addCart(product));
                 }}
                 className="inline-block flex-1 rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 sm:flex-none md:text-base"
               >
