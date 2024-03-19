be0feb67983750a9af251e68a0ba2c5310f741fd
Maherun Nessa <meherunela2002@gmail.com>
2024-03-19 13:12:13 +0600

    solve

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import HomeHero from "../../../components/HomeHero";
-import HomeTeam from "../../../components/HomeTeam";
-import HomeLatest from "../../../components/HomeLatest";
-import HomeTestimonial from "../../../components/HomeTestimonial";
-import HomeArtist from "../../../components/HomeArtist";
+import HomeHero from "./components/home/components/HomeHero";
+import HomeTeam from "./components/home/components/HomeTeam";
+import HomeLatest from "./components/home/components/HomeLatest";
+import HomeTestimonial from "./components/home/components/HomeTestimonial";
+import HomeArtist from "./components/home/components/HomeArtist";
 
 export default function Home() {
   return (
