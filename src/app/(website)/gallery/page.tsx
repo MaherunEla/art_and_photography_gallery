0fed4f9515f35f135e4b3aa82efda8177ee65aa1
Maherun Nessa <meherunela2002@gmail.com>
2024-03-19 13:12:13 +0600

    solve

--- src/app/(website)/gallery/page.tsx
+++ src/app/(website)/gallery/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { HomeGalleryData } from "../../../../components/HomeGallery";
+import { HomeGalleryData } from "../components/home/components/HomeGallery";
 
 const Gallerypage = () => {
   return (
