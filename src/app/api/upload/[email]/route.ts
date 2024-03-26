71301d0e6dc433a3a146e98cf381d4c3bbdd32ed
Maherun Nessa <meherunela2002@gmail.com>
2024-03-27 00:42:18 +0600

    dynamic mygallery

--- src/app/api/upload/[email]/route.ts
+++ src/app/api/upload/[email]/route.ts
@@ -5,16 +5,24 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 const prisma = new PrismaClient();
 
-export const GET = async (req: any) => {
-  const uploads = await prisma.upload.findMany();
+export const GET = async (req: any, { params }: any) => {
+  const uploads = await prisma.upload.findMany({
+    where: {
+      userEmail: params.id as string,
+    },
+  });
   return NextResponse.json(uploads);
 };
 
-export async function POST(req: Request) {
+export async function POST(req: Request, { params }: any) {
+  console.log(params);
   const data = await req.json();
 
   const res = await prisma.upload.create({
-    data: data,
+    data: {
+      ...data,
+      userEmail: params?.email as string,
+    },
   });
   return NextResponse.json(res);
 }
