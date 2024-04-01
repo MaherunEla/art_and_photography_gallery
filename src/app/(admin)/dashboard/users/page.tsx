e9f55dade39993ee12b9591e56056bf7cc6c68f4
Maherun Nessa <meherunela2002@gmail.com>
2024-04-01 16:42:07 +0600

    befor pagination

--- src/app/(admin)/dashboard/users/page.tsx
+++ src/app/(admin)/dashboard/users/page.tsx
@@ -9,24 +9,30 @@ import Link from "next/link";
 import axios from "axios";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 
+const fetchUpload = async () => {
+  const { data } = await axios.get("/api/signup");
+  return data;
+};
 const Userpage = () => {
-  const fetchUpload = () => {
-    return axios.get(`/api/signup/`);
-  };
-
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery({
     queryKey: ["signup-data"],
     queryFn: fetchUpload,
   });
 
-  console.log(data?.data);
-  const tabledata = data?.data || [];
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{(error as any).message}</h2>;
+  }
+
+  const tabledata = data || [];
 
   return (
     <div className="bg-[#182237] p-5 rounded-[10px] mt-5">
       <div className="flex items-center justify-between">
         <Search placeholder="Search for a user ..." />
-        <Link href="/">
+        <Link href="/dashboard/users/add">
           <button className="p-[10px] bg-[#5d57c9] text-white border-none rounded-[5px] cursor-pointer">
             Add New
           </button>
---
+++ src/app/(admin)/dashboard/users/view/[id]/page.tsx
@@ -0,0 +1,293 @@
+"use client";
+import { useToast } from "@/components/ui/use-toast";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { TbMoodEdit } from "react-icons/tb";
+import { z } from "zod";
+
+const signupformSchema = z.object({
+  name: z.string().min(1, "Name is required"),
+
+  contact: z
+    .string()
+    .min(1, "Contact is required")
+    .length(11, "Must Contain 11 Digit"),
+  email: z
+    .string()
+    .min(1, "Email is required")
+    .email({ message: "Invalid Email address" }),
+  password: z.string().min(8, "Password is required"),
+
+  occupation: z.string().min(1, "occupation is required"),
+  image: z.string().min(1, "Image is reguired"),
+  role: z.string(),
+
+  socialaccountf: z.string().optional(),
+  socialaccountl: z.string().optional(),
+});
+
+type FormValues = z.infer<typeof signupformSchema>;
+
+const Viewpage = () => {
+  const params = useParams();
+
+  const encodedEmail = params.id as string;
+  const decodedEmail = decodeURIComponent(encodedEmail);
+
+  console.log(params.id);
+
+  const fetchProfile = async () => {
+    const { data } = await axios.get(`/api/signup/${decodedEmail}`);
+    return data;
+  };
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery({
+    queryKey: ["signup-data"],
+    queryFn: fetchProfile,
+  });
+
+  console.log({ data });
+  const [image, setImage] = useState(data?.image);
+
+  const form = useForm<FormValues>({
+    defaultValues: async () => {
+      const { data } = await axios.get(`/api/signup/${decodedEmail}`);
+      return data;
+    },
+    resolver: zodResolver(signupformSchema),
+  });
+
+  const {
+    handleSubmit,
+    register,
+    control,
+    setValue,
+    formState: { errors },
+  } = form;
+
+  const uploadImages = async (
+    file: File
+  ): Promise<{ success: number; url: string | null }> => {
+    try {
+      let formData = new FormData();
+      formData.append("file", file);
+      formData.append("upload_preset", "my-uploads");
+
+      const res = await axios.post(
+        "https://api.cloudinary.com/v1_1/djfwg1dfa/image/upload",
+        formData
+      );
+      console.log({ res });
+
+      return {
+        success: 1,
+        url: res.data.secure_url,
+      };
+    } catch (error) {
+      console.error({ error });
+      return {
+        success: 1,
+        url: null,
+      };
+    }
+  };
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const onSubmit = async (data: FormValues) => {
+    console.log("Form submitted...", data);
+    try {
+      const res = await axios.put(
+        `http://localhost:3000/api/signup/${decodedEmail}`,
+        data
+      );
+      console.log({ res });
+      refetch();
+      queryClient.invalidateQueries({ queryKey: ["signup-data"] });
+      toast({
+        title: "Updated successfully",
+      });
+    } catch (err) {
+      console.error("Error updating profile:", err);
+      // Handle error gracefully
+    }
+  };
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{(error as any).message}</h2>;
+  }
+  return (
+    <div className="flex gap-[50px] mt-5">
+      <div className="flex-1 bg-[#182237] p-5 rounded-[10px] font-bold text-[#b7bac1] max-h-max">
+        <div className="w-[250px] h-[250px] md:w-[350px] md:h-[350px]  lg:w-[400px] lg:h-[400px] relative rounded-[10px] overflow-hidden mb-5">
+          <Image src={image || data?.image} alt="" fill />
+        </div>
+        {data?.name}
+      </div>
+      <div className="flex-3 bg-[#182237] p-5 rounded-[10px]">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+          <div className="flex items-center justify-center pt-5"></div>
+          <div className="mx-auto grid max-w-screen-md gap-4 sm:grid-cols-2">
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Name
+              </label>
+              <input
+                type="text"
+                {...register("name")}
+                // defaultValue={form.getValues("name")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.name && (
+                <p className="text-red-600">{errors.name.message as string}</p>
+              )}
+            </div>
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold  text-white sm:text-base">
+                Contact
+              </label>
+              <input
+                type="text"
+                {...register("contact")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.contact && (
+                <p className="text-red-600">
+                  {errors.contact.message as string}
+                </p>
+              )}
+            </div>
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Email
+              </label>
+              <input
+                type="text"
+                {...register("email")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.email && (
+                <p className="text-red-600">{errors.email.message as string}</p>
+              )}
+            </div>
+
+            <div className="sm:col-span-2 ">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Password
+              </label>
+              <input
+                type="text"
+                {...register("password")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.password && (
+                <p className="text-red-600">
+                  {errors.password.message as string}
+                </p>
+              )}
+            </div>
+
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Image
+              </label>
+              <Controller
+                name="image"
+                control={control}
+                render={({ field }) => (
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none "
+                    onChange={async (e: any) => {
+                      const file = e.target.files[0];
+                      const res: any = await uploadImages(file);
+                      console.log("res", res);
+                      setValue("image", res.url);
+                      setImage(res.url);
+                      field.onChange(res.url);
+                    }}
+                  />
+                )}
+              />
+              {errors.image && (
+                <p className="error">{errors.image.message as string}</p>
+              )}
+            </div>
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Occupation
+              </label>
+              <input
+                type="text"
+                {...register("occupation")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.occupation && (
+                <p className="text-red-600">
+                  {errors.occupation.message as string}
+                </p>
+              )}
+            </div>
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Role
+              </label>
+              <input
+                type="text"
+                {...register("role")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.role && (
+                <p className="text-red-600">{errors.role.message as string}</p>
+              )}
+            </div>
+            <div className="sm:col-span-2">
+              <label className="mb-2 inline-block text-lg font-bold text-white sm:text-base">
+                Social Accunts
+              </label>
+              <input
+                type="text"
+                placeholder="Twiter"
+                {...register("socialaccountf")}
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none mb-5"
+              />
+              {errors.socialaccountf && (
+                <p className="text-red-600">
+                  {errors.socialaccountf.message as string}
+                </p>
+              )}
+              <input
+                type="text"
+                {...register("socialaccountl")}
+                placeholder="LinkedIn"
+                className="w-full px-2 py-2 bg-[#151c2c] text-white border-2 border-[#2e374a] rounded-[5px] outline-none"
+              />
+              {errors.socialaccountl && (
+                <p className="text-red-600">
+                  {errors.socialaccountl.message as string}
+                </p>
+              )}
+            </div>
+            <div className="flex items-center justify-between sm:col-span-2">
+              <button
+                type="submit"
+                className="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base"
+              >
+                Update
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Viewpage;
